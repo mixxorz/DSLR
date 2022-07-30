43f554327c89f3b0db0932cf8a8e319cc49e51cb
Mitchel Cabuloy <mixxorz@gmail.com>
2022-07-30 09:16:51 +0800

    Extract exec to runner module

--- dslr/operations.py
+++ dslr/operations.py
@@ -1,45 +1,8 @@
-import os
-import subprocess
-from collections import namedtuple
 from time import time
 from typing import List
 
 from .config import settings
-
-
-Result = namedtuple("Result", ["returncode", "stdout", "stderr"])
-Snapshot = namedtuple("Snapshot", ["dbname", "name", "timestamp"])
-
-
-def exec(cmd: str) -> Result:
-    """
-    Executes a command.
-    """
-
-    # Set PG environment variables based on the settings
-    env = os.environ.copy()
-    env["PGHOST"] = settings.db.host or env.get("PGHOST", "")
-    env["PGPORT"] = str(settings.db.port) or env.get("PGPORT", "")
-    env["PGUSER"] = settings.db.username or env.get("PGUSER", "")
-    env["PGPASSWORD"] = settings.db.password or env.get("PGPASSWORD", "")
-
-    if settings.debug:
-        print(f"COMMAND: {cmd}")
-
-    with subprocess.Popen(
-        cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=env, shell=True
-    ) as p:
-        stdout, stderr = p.communicate()
-
-        if settings.debug:
-            print("STDOUT:\n", stdout.decode("utf-8"), "\n")
-            print("STDERR:\n", stderr.decode("utf-8"), "\n")
-
-        return Result(
-            returncode=p.returncode,
-            stdout=stdout.decode("utf-8"),
-            stderr=stderr.decode("utf-8"),
-        )
+from .runner import Snapshot, exec
 
 
 class DSLRException(Exception):
